7a34f5dd31f5ccc7e5bc09961648db64079e10b0
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-01 22:16:11 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from crontab import CronTab
-cron = CronTab(user=True)
+cron = CronTab(user="username")
 detectIPjob = cron.new(command="python3 Detect-IP.py")
+detectIPjob.hour.on(24,1,2,3,4,5,6)
 cron.write()
