3d21d95bbb05aade97a6249dd1af03ebd3d4fe88
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-01 22:16:09 -0400

    Update Detect-IP.py

--- Detect-IP.py
+++ Detect-IP.py
@@ -1,1 +1,18 @@
-
+import os
+import datetime
+import re
+dt = datetime.datetime.now()
+monthAbrv = dt.strftime("%b %d")
+if re.match("0*",monthAbrv):
+    monthAbrv = monthAbrv.replace("0", " ")
+    print(monthAbrv)
+fOpen = open("/var/log/secure","r")
+if monthAbrv in fOpen:
+    redFladFind = re.findall(monthAbrv,fOpen)
+    redFlagFile = open("~/redflags.txt","a")
+    redFlagFile.write(redFladFind)
+    redFlagFile.close()
+os.system("tcpdump -w icmplog.pcap -i eth0 icmp")
+os.system("tcpdump -w sshlog.pcap -i eth0 port 22")
+os.system("tcpdump -w httplog.pcap -i eth0 port 80")
+os.system("tcpdump -w httpslog.pcap -i eth0 port 443")
