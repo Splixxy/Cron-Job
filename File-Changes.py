ab3ec1c604dd1de08440ef444308b15990343a09
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-03 19:46:36 -0400

    Update File-Changes.py

--- File-Changes.py
+++ File-Changes.py
@@ -1,1 +1,17 @@
+#cron job to run on startup
+import os
+from inotify_simple import INotify, flags
+inotify = INotify()
+watch_flags = flags.CREATE | flags.DELETE | flags.MODIFY | flags.DELETE_SELF
+wdVarLog = inotify.add_watch("/var/log", watch_flags)
+wdETC = inotify.add_watch("/etc", watch_flags)
 
+for event in inotify.read():
+    fOpen = open("~/directorylogs.txt","a")
+    fOpen.write(event)
+    fOpen.close()
+    for flag in flags.from_mask(event.mask):
+        fstring = (" " + str(flag))
+        fOpen = open("~/directorylogs.txt","a")
+        fOpen.write(fstring)
+        fOpen.close()
