04082dd38ee7c25795725f15676cdf4af8ad5610
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-04 18:19:51 -0400

    Update Detect-IP.py

--- Detect-IP.py
+++ Detect-IP.py
@@ -1,17 +1,23 @@
+#import needed packages
 import os
 import datetime
-import re
+#pulls in the datetime package
 dt = datetime.datetime.now()
-monthAbrv = dt.strftime("%b %d")
-if re.match("0*",monthAbrv):
-    monthAbrv = monthAbrv.replace("0", " ")
+#sets strings for the abreviated month and day and hour in military time
+monthAbrv = dt.strftime("%b%d")
+monthAbrv = str(monthAbrv)
+hour = dt.strftime("%H")
+hour = str(hour)
+#opens /var/log/secure in read mode
 fOpen = open("/var/log/secure","r")
-if monthAbrv in fOpen:
-    redFladFind = re.findall("%s*",fOpen % monthAbrv)
-    redFlagFile = open("~/redflags.txt","a")
-    redFlagFile.write(redFladFind)
-    redFlagFile.close()
-os.system("tcpdump -w icmplog.pcap -i eth0 icmp")
-os.system("tcpdump -w sshlog.pcap -i eth0 port 22")
-os.system("tcpdump -w httplog.pcap -i eth0 port 80")
-os.system("tcpdump -w httpslog.pcap -i eth0 port 443")
+#sets fRead as the output of fOpen.read
+fRead = fOpen.read()
+#opens a file and then writes fRead to the file
+redFlagFile = open("tcpDlogs/redflags%s%s.txt" % (monthAbrv, hour),"a")
+redFlagFile.write(fRead)
+redFlagFile.close()
+#starts tcpdump for icmp, port 22, 80, and 443
+os.system("tcpdump -w tcpDlogs/icmplog-%s-%s.pcap -i any -c10 icmp" % (monthAbrv, hour))
+os.system("tcpdump -w tcpDlogs/sshlog-%s-%s.pcap -i any -c10 port 22" % (monthAbrv, hour))
+os.system("tcpdump -w tcpDlogs/httplog-%s-%s.pcap -i any -c10 port 80" % (monthAbrv, hour))
+os.system("tcpdump -w tcpDlogs/httpslog-%s-%s.pcap -i any -c10 port 443" % (monthAbrv, hour))
