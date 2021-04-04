d7710b5b914a965b16b47d2ed0672ed37ed592a7
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-04 18:19:55 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
+#imports contab package
 from crontab import CronTab
-cron = CronTab(user="username")
+#creates the cron job of Detect-IP.py script to run every hour
+cron = CronTab(user="root")
 detectIPjob = cron.new(command="python3 Detect-IP.py")
-detectIPjob.hour.on(24,1,2,3,4,5,6)
+detectIPjob.hour.every(1)
+cron.write()
+#creates the cron job of Backup.py to run every Friday
+cron = CronTab(user="root")
+Backupjob = cron.new(command="python3 Backup.py")
+Backupjob.day.on(5)
 cron.write()
