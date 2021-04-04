4ba66370809df258626bb12e36018aeb9cea12d0
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-04 18:19:49 -0400

    Update Backup.py

--- Backup.py
+++ Backup.py
@@ -1,4 +1,8 @@
+#import the os package
 import os
+#installs gzip
 os.system("%s install gzip" % distro)
+#tars the / directory
 os.system("tar -zcvf Full-Backup.tar.gz /*")
+#prints out that it has been backupped
 print("/ has been backupped.")
